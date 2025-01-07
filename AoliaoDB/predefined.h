eba970c0215e4038a5c62e0a07feb885810d96ca
gaoguuu <2498638976@qq.com>
2025-01-07 13:24:11 +0800

    windows

---
+++ AoliaoDB/predefined.h
@@ -0,0 +1,55 @@
+#pragma once
+#ifndef PREDEFINED_H
+#define PREDEFINED_H
+#include<iostream>
+#include <string.h>
+
+namespace bpt {
+
+ /* predefined B+ info */
+#define BP_ORDER 50
+
+/* key/value type */
+
+    struct value_t {
+        char name[256];
+        int age;
+        char email[256];
+    };
+
+    // typedef int value_t;
+    struct key_t {
+        char k[16];
+
+        key_t(const char* str = "")
+        {
+
+            memset(k, 0, sizeof(k));
+            //bzero(k, sizeof(k));
+            strcpy_s(k, str);
+        }
+    };
+
+    inline int keycmp(const key_t& a, const key_t& b) {
+       
+        int x = strlen(a.k) - strlen(b.k);
+        return x == 0 ? strcmp(a.k, b.k) : x;
+    }
+
+#define OPERATOR_KEYCMP(type) \
+    bool operator< (const key_t &l, const type &r) {\
+        return keycmp(l, r.key) < 0;\
+    }\
+    bool operator< (const type &l, const key_t &r) {\
+        return keycmp(l.key, r) < 0;\
+    }\
+    bool operator== (const key_t &l, const type &r) {\
+        return keycmp(l, r.key) == 0;\
+    }\
+    bool operator== (const type &l, const key_t &r) {\
+        return keycmp(l.key, r) == 0;\
+    }
+
+}
+
+#endif /* end of PREDEFINED_H */
